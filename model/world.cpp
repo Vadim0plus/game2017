d86cc665e59abe4b816e81c5f9b92c06404d430f
Valeria Kulikova <lera_kulikova@mail.ru>
2017-07-18 13:05:56 +0300

    createTestsWorldClass

--- model/world.cpp
+++ model/world.cpp
@@ -52,3 +52,57 @@ void World::swap( World* p_World )
 {
         memcpy( p_Cells, p_World->p_Cells, m_Width * m_Height * sizeof( byte ) );
 }
+
+void WorldTest::TestGetWidth()
+{
+    World worldtest = new World(10,10);
+    int ans1 = 10;
+    int ans2 = worldtest.getWidth();
+    if ( ans1 == ans2)
+        std::cout << "TestGetWidth:OK\n"
+    else
+                     std::cout << "TestGetWidth:FALSE\n"
+
+}
+
+void WorldTest::TestGetHeight();
+    {
+        World worldtest = new World(15,10);
+        int ans3=10;
+        int ans4=worldtest.getHeight();
+        if (ans3 == ans4)
+            std::cout << "TestGetHeight:OK\n"
+else
+                         std::cout << "TestGetHeight:FALSE\n"
+
+    }
+
+ void WorldTest::TestAt();
+        {
+        World worldtest = new World(15,10);
+        p_wtestorld->set( 6, 1, 1 ); p_World->set( 7, 2, 1 );
+        p_worldtest->set( 5, 3, 1 ); p_World->set( 6, 3, 1 );
+        p_worldtest->set( 7, 3, 1 );
+        int ans1=1;
+        int ans2=WorldTest.at();
+        if (ans1 == ans2)
+            std::cout << "TestAt:OK\n"
+else
+                         std::cout << "TestAt:FALSE\n"
+
+        }
+
+void WorldTest::TestSet();
+        {
+            World worldtest = new World(10,10);
+            WorldTest.set(1,1,1);
+           int ans1=1;
+           int ans2=worldtest.at(1,1)
+
+           if (ans1 == ans2)
+               std::cout << "TestSet:OK\n"
+else
+                            std::cout <<"TestSet:FALSE\n"
+
+        }
+
